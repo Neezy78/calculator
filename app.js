ac4d2e56e8c13103084768c0230c9f4e8b5f9c5f
Neez <leolepage@outlook.fr>
2023-12-20 16:36:20 +0100

    removing the possibility to type more than one dot

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 // dom
 const headerContent = document.querySelector("#header-content");
 const calculating = document.querySelector("#calculating");
+
+// regex used for filtering buttons used
 const regex = /[\d(\.)]/;
 const mathregex = /[-+*%\/]/;
 
+// check if it is the result or if a dot has been typed.
 let isResult = false;
+let typedOneDot = false;
 
+// used for storing the calculating data
 let calculatingArr = [];
+
 // buttons
 const buttons = document.querySelectorAll(".calc-btn");
 
 buttons.forEach((button) =>
   button.addEventListener("click", function (e) {
-    if (headerContent.textContent.length < 10) {
+    if (headerContent.textContent.length < 15) {
       if (regex.test(e.target.textContent)) {
         if (!isResult) {
-          headerContent.textContent += e.target.textContent;
+          if (e.target.textContent === ".") {
+            if (!typedOneDot) {
+              headerContent.textContent += e.target.textContent;
+              typedOneDot = true;
+            } else {
+            }
+          } else {
+            headerContent.textContent += e.target.textContent;
+          }
         }
       }
       // check if target is +
@@ -27,6 +41,7 @@ buttons.forEach((button) =>
         result(calculatingArr);
       }
     }
+
     // reset btn
     if (e.target.textContent === "AC") {
       reset();
@@ -39,17 +54,22 @@ function reset() {
   calculating.textContent = "";
   calculatingArr = [];
   isResult = false;
+  typedOneDot = false;
 }
 
 function adding(numbers, operator) {
+  typedOneDot = false;
   isResult = false;
-  // first array
+
+  //avoid using too much space
   if (calculating.textContent.length > 20) {
     calculating.textContent = "";
   }
+  // adding the values to the calculating array
   calculatingArr.push(numbers + operator);
-  console.log(calculatingArr);
+  // changing the top of the header with the values
   calculating.textContent += headerContent.textContent + operator;
+  // reset input
   headerContent.textContent = "";
 }
 
